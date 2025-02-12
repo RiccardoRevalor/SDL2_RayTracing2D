0e0af23ff00862a0ca0f663579a1eec9ef08a3a8
RiccardoRevalor <riccardorevalor@gmail.com>
2025-02-12 01:28:09 +0100

    circle, basic geometry funcs

--- 2d_raytracing.c
+++ 2d_raytracing.c
@@ -1,5 +1,7 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <math.h>
+
 
 //window size
 #define WINDOW_HEIGHT 900
@@ -7,6 +9,22 @@
 
 //colors
 #define WHITE 0xffffffff
+
+
+//structs
+
+
+typedef struct {
+    float centerX, centerY;         //Circle center
+    float rad;                     //circle radius
+} Circle;
+
+
+
+//Funzioni custom
+void drawCircle(SDL_Surface* surface , Circle circle, Uint32 color);
+float distanceSquared(float aX, float aY, float bX, float bY);
+int belongsToCircle(Circle circle, float aX, float aY);
 
 int main(int argc, char* argv[]) {
     //inizializzazione di SDL
@@ -26,16 +44,61 @@ int main(int argc, char* argv[]) {
     SDL_FillRect(surface, &rect, color)
     rect è il puntatore ad un oggetto SDL_Rect
     SDL_Rect lo ottieni castando a SDL_Rect un array del tipo {x, y, width, heigth}
-    */
-
+    
     SDL_Rect rect = (SDL_Rect) {200, 200, 200, 200};
     SDL_FillRect(surface, &rect, WHITE);
+    */
+    
+    Circle circle0 = {200, 200, 20};
+    drawCircle(surface, circle0, WHITE);
+    
+    
+    
+    
     
     //aggiorna la window
     SDL_UpdateWindowSurface(window);
-    
-    
     SDL_Delay(5000);
 
     return 0;
+}
+
+
+void drawCircle(SDL_Surface* surface , Circle circle, Uint32 color){
+    /*
+    Function do draw and fill a circle on the surface   
+    */
+
+
+    float x, y;
+
+    //Just loop through the rectangle containing the circle to understand which points of it actually belong to the circle too!
+    for (x = circle.centerX - circle.rad; x <= circle.centerX + circle.rad; x++){
+        for (y = circle.centerY - circle.rad; y <= circle.centerY + circle.rad; y++){
+            //if the pixel located at (x,y) belongs the circle, draw it
+            if (belongsToCircle(circle, x, y)){
+                SDL_Rect pixel = (SDL_Rect) {x, y, 1, 1};
+                SDL_FillRect(surface, &pixel, WHITE);
+            }
+        }
+    }  
+
+}
+
+
+float distanceSquared(float aX, float aY, float bX, float bY){
+    /* Return Euclidean distance squared*/
+
+    return pow(aX - bX, 2) + pow(aY - bY, 2);
+}
+
+
+int belongsToCircle(Circle circle, float aX, float aY){
+    /* Boolean Function, return 1 if point a belongs to Circle circle, otherwise return 0*/
+
+    float radSquared = pow(circle.rad, 2);
+    if (distanceSquared(aX, aY, circle.centerX, circle.centerY) <= radSquared) return 1;
+
+    return 0;
+
 }
